1c5fe272b5cb45515a8a24aac0295b4f4760c285
Jacob Helwig <jacob.helwig@gmail.com>
2009-09-27 15:52:13 -0700

    Use app user to deploy, and don't use sudo.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,19 @@
 require File.join(File.dirname(__FILE__), 'capistrano_database')
 
-set :application, "kamitsukai"
-set :repository,  "git@github.com:jhelwig/kamitsukai.git"
+set :application, 'kamitsukai'
+set :repository,  'git@github.com:jhelwig/kamitsukai.git'
+set :scm,         :git
+set :user,        'app'
+set :use_sudo,    false
 
-set :scm, :git
-
-role :web, "kamitsukai.perlninja.com"                          # Your HTTP server, Apache/etc
-role :app, "kamitsukai.perlninja.com"                          # This may be the same as your `Web` server
-role :db,  "kamitsukai.perlninja.com", :primary => true # This is where Rails migrations will run
-#role :db,  "your slave db-server here"
+role :web, 'kamitsukai.perlninja.com'                   # Your HTTP server, Apache/etc
+role :app, 'kamitsukai.perlninja.com'                   # This may be the same as your `Web` server
+role :db,  'kamitsukai.perlninja.com', :primary => true # This is where Rails migrations will run
+#role :db,  'your slave db-server here'
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
-set :branch, "master"
+set :branch, 'master'
 set :deploy_via, :remote_cache
 set :git_enable_submodules, 1
 
@@ -33,6 +34,6 @@ namespace :deploy do
   task :stop do
   end
   task :restart, :roles => :app, :except => { :no_release => true } do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
   end
 end
