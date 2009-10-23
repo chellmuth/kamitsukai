a0e43f4b4f6d1c83b2815118976a27e997c73c7b
Jacob Helwig <jacob.helwig@gmail.com>
2009-10-22 22:11:41 -0700

    Add missing gem dependency.

--- config/environment.rb
+++ config/environment.rb
@@ -45,6 +45,7 @@ Rails::Initializer.run do |config|
   config.gem 'newrelic_rpm', :version => '2.9.5'
   config.gem 'notahat-machinist', :lib => 'machinist', :source => 'http://gems.github.com'
   config.gem 'pg', :version => '0.8.0'
+  config.gem 'relevance-rcov', :lib => 'rcov', :source => 'http://gems.github.com'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
