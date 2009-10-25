6845d35c6d30ecf6ef3f5a966447829b4ca8c6ec
Jacob Helwig <jacob.helwig@gmail.com>
2009-10-24 19:54:45 -0700

    Update watchr.rb script to support views.

--- config/watchr.rb
+++ config/watchr.rb
@@ -1,39 +1,33 @@
-watch('app/models/(.*)\.rb') do |md|
-  test_file = "spec/models/#{md[1]}_spec.rb"
-
+def run_spec_file(test_file)
   puts "\n\n"
   system "date"
   puts test_file
 
   system %Q{./script/spec '#{test_file}' --diff unified --format nested --color}
+end
+
+watch('app/models/(.*)\.rb') do |md|
+  test_file = "spec/models/#{md[1]}_spec.rb"
+
+  run_spec_file test_file
 end
 
 watch('app/controllers/(.*)\.rb') do |md|
   test_file = "spec/controllers/#{md[1]}_spec.rb"
 
-  puts "\n\n"
-  system "date"
-  puts test_file
+  run_spec_file test_file
+end
 
-  system %Q{./script/spec '#{test_file}' --diff unified --format nested --color}
+watch('app/views/(.*)') do |md|
+  test_file = "spec/views/#{md[1]}_spec.rb"
+
+  if File.exists?(test_file)
+    run_spec_file test_file
+  end
 end
 
 watch('spec/.*_spec\.rb') do |md|
   test_file = "#{md[0]}"
 
-  puts "\n\n"
-  system "date"
-  puts test_file
-
-  system %Q{./script/spec '#{test_file}' --diff unified --format nested --color}
-end
-
-watch('test/.*_test\.rb') do |md|
-  test_file = "#{md[0]}"
-
-  puts "\n\n"
-  system "date"
-  puts test_file
-
-  system %Q{ruby1.8 -rubygems -e "require 'redgreen'" -I.:lib:test -e "require '#{test_file}'"}
+  run_spec_file test_file
 end
