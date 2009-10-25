1a86e0b7249087d49e0451b7218ff69461dd2a6f
Jacob Helwig <jacob.helwig@gmail.com>
2009-10-24 19:54:40 -0700

    Migrate Admin::SettingsControler tests to RSpec

---
+++ spec/controllers/admin/settings_controller_spec.rb
@@ -0,0 +1,318 @@
+require 'spec_helper'
+
+describe Admin::SettingsController do
+  it "should use Admin::SettingsController" do
+    controller.should be_an_instance_of(Admin::SettingsController)
+  end
+
+
+  describe 'without a logged in user' do
+    it 'should redirect to the login page' do
+      get :index
+      response.should redirect_to '/login'
+    end
+  end
+
+  describe 'with a logged in user' do
+    before(:each) do
+      @user = login
+    end
+
+    it 'should redirect home' do
+      get :index
+      response.should redirect_to '/'
+    end
+
+    describe 'that is an administrattor' do
+      before(:each) do
+        @user.has_role :administrator
+      end
+
+      it 'should allow access' do
+        get :index
+        response.should be_success
+      end
+
+      describe 'without existing settings' do
+        describe 'the empty index' do
+          it 'should be successful' do
+            get :index
+            response.should be_success
+          end
+
+          it 'should not list any settings' do
+            get :index
+            assigns(:settings).should be_empty
+          end
+        end
+      end
+
+      describe 'with existing settings' do
+        before(:each) do
+          @settings = []
+          (1..5).to_a.each do
+            @settings.push(Setting.make)
+          end
+        end
+
+        after(:each) do
+          @settings.each {|s| s.destroy}
+        end
+
+        describe 'the index' do
+          it 'should be successful' do
+            get :index
+            response.should be_success
+          end
+
+          it 'should assign settings' do
+            get :index
+            assigns(:settings).should_not be_empty
+          end
+        end
+
+        describe "GET 'show'" do
+          it 'should be successful' do
+            setting = @settings.rand
+
+            get :show, {:id => setting.id}
+
+            response.should be_succes
+          end
+
+          it 'should assign setting' do
+            setting = @settings.rand
+
+            get :show, {:id => setting.id}
+
+            assigns(:setting).should_not be_nil
+            assigns(:setting).should == setting
+          end
+        end
+      end
+
+      describe 'with invalid Setting options' do
+      end
+
+      describe 'with valid Setting options' do
+        before(:each) do
+          @setting = Setting.make
+        end
+
+        after(:each) do
+          @setting.destroy
+        end
+
+        describe "GET 'new'" do
+          before(:each) do
+            Setting.stub!(:new).and_return(@new_setting = mock_model(Setting, :save => true))
+          end
+
+          it "should be successful" do
+            get :new
+            response.should be_success
+          end
+
+          it 'should assign setting' do
+            get :new
+            assigns(:setting).should == @new_setting
+          end
+        end
+
+        describe "PUT 'update'" do
+          def do_good_update
+            put :update, {
+              :id => @setting.id,
+              :setting => {
+                :key   => @setting.key + 'abcd',
+                :value => @setting.value + 'abcd'
+              }
+            }
+            @setting.reload
+          end
+
+          it 'should redirect to the setting' do
+            do_good_update
+            response.should redirect_to admin_setting_url(@setting)
+          end
+
+          it 'should set a success flash message' do
+            do_good_update
+            flash[:notice].should == 'Successfully updated setting.'
+          end
+
+          it 'should not increase the Setting.count' do
+            do_good_update
+            Setting.should have(1).records
+          end
+
+          it 'should assign setting' do
+            do_good_update
+            assigns(:setting).should == @setting
+          end
+        end
+
+        describe "GET 'edit'" do
+          it "should be successful" do
+            get :edit, {:id => @setting.id}
+            response.should be_success
+          end
+
+          it 'should assign setting' do
+            get :edit, {:id => @setting.id}
+            assigns(:setting).should == @setting
+          end
+        end
+
+        describe "PUT 'create'" do
+          it 'should redirect to settings list' do
+            put :create, { :setting => Setting.plan }
+            response.should redirect_to admin_settings_url
+          end
+
+          it 'should create a new setting' do
+            lambda {
+              put :create, { :setting => Setting.plan }
+            }.should change{ Setting.count }.from(1).to(2)
+          end
+
+          it 'should set a success flash message' do
+            put :create, { :setting => Setting.plan }
+            flash[:notice].should == 'Setting successfully created.'
+          end
+        end
+
+        describe "DELETE 'destroy'" do
+          it 'should redirect back to the settings list' do
+            delete :destroy, { :id => @setting.id }
+            response.should redirect_to admin_settings_url
+          end
+
+          it 'should set a success flash message' do
+            delete :destroy, { :id => @setting.id }
+            flash[:notice].should == 'Setting successfully removed.'
+          end
+
+          it 'should destroy the setting' do
+            setting_id = @setting.id
+
+            delete :destroy, { :id => @setting }
+            lambda {
+              Setting.find(setting_id)
+            }.should raise_error(ActiveRecord::RecordNotFound)
+          end
+        end
+      end
+
+      describe 'with invalid Setting options' do
+        before(:each) do
+          @setting = Setting.make
+        end
+
+        after(:each) do
+          @setting.destroy
+        end
+
+        describe "PUT 'update'" do
+          def do_bad_update
+            put :update, {
+              :id => @setting.id,
+              :setting => {
+                :key   => nil,
+                :value => @setting.value
+              }
+            }
+            @setting.reload
+          end
+
+          it 'should be successful' do
+            do_bad_update
+            response.should be_success
+          end
+
+          it 'should render the edit template' do
+            do_bad_update
+            response.should render_template('edit')
+          end
+
+          it 'should not increase the Setting.count' do
+            do_bad_update
+            Setting.should have(1).records
+          end
+
+          it 'should assign setting' do
+            do_bad_update
+            assigns(:setting).should == @setting
+          end
+
+          it 'should not update the setting' do
+            lambda {
+              do_bad_update
+            }.should_not change{@setting.key}
+          end
+        end
+
+        describe "GET 'edit'" do
+          it "should not be successful" do
+            lambda {
+              get :edit, {:id => @setting.id+1}
+            }.should raise_error(ActiveRecord::RecordNotFound)
+          end
+        end
+
+        describe "PUT 'create'" do
+          def do_bad_create
+            put :create, {
+              :setting => {
+                :key   => nil,
+                :value => @setting.value
+              }
+            }
+          end
+
+          it 'should be successful' do
+            do_bad_create
+            response.should be_success
+          end
+
+          it 'should render the new template' do
+            do_bad_create
+            response.should render_template('new')
+          end
+
+          it 'should not create a new setting' do
+            lambda {
+              do_bad_create
+            }.should_not change{ Setting.count }
+          end
+        end
+
+        describe "DELETE 'destroy'" do
+          def do_bad_delete
+              delete :destroy, { :id => @setting.id+1 }
+          end
+          it 'should be successful' do
+            do_bad_delete
+            response.should redirect_to admin_settings_url
+          end
+
+          it 'should set a failure flash message' do
+            do_bad_delete
+            flash[:notice].should == 'Unable to remove setting.'
+          end
+
+          it 'should not destroy any setting' do
+            lambda {
+              do_bad_delete
+            }.should_not change{ Setting.count }
+          end
+        end
+      end
+    end
+
+    after(:each) do
+      logout
+      @user.destroy
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,9 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
 require 'spec/autorun'
 require 'spec/rails'
 require File.expand_path(File.join(File.dirname(__FILE__),'blueprints.rb'))
+require 'authlogic/test_case'
 
 # Uncomment the next line to use webrat's matchers
 #require 'webrat/integrations/rspec-rails'
@@ -14,46 +13,25 @@ require File.expand_path(File.join(File.dirname(__FILE__),'blueprints.rb'))
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
 Spec::Runner.configure do |config|
-  # If you're not using ActiveRecord you should remove these
-  # lines, delete config/database.yml and disable :active_record
-  # in your config/boot.rb
   config.use_transactional_fixtures = true
-  config.use_instantiated_fixtures  = false
-  config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
 
-  # == Fixtures
-  #
-  # You can declare fixtures for each example_group like this:
-  #   describe "...." do
-  #     fixtures :table_a, :table_b
-  #
-  # Alternatively, if you prefer to declare them only once, you can
-  # do so right here. Just uncomment the next line and replace the fixture
-  # names with your fixtures.
-  #
-  # config.global_fixtures = :table_a, :table_b
-  #
-  # If you declare global fixtures, be aware that they will be declared
-  # for all of your examples, even those that don't use them.
-  #
-  # You can also declare which fixtures to use (for example fixtures for test/fixtures):
-  #
-  # config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
-  #
-  # == Mock Framework
-  #
-  # RSpec uses it's own mocking framework by default. If you prefer to
-  # use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  #
-  # == Notes
-  #
-  # For more information take a look at Spec::Runner::Configuration and Spec::Runner
+  config.before(:all)  {
+    Sham.reset(:before_all)
+  }
 
-  config.before(:all)  { Sham.reset(:before_all)  }
-  config.before(:each) { Sham.reset(:before_each) }
+  config.before(:each) {
+    Sham.reset(:before_each)
+    activate_authlogic
+  }
+end
 
+def login(user_info=nil)
+  user = User.make(user_info)
+  assert UserSession.create(user)
+  user
+end
+
+def logout
+  session = UserSession.find
+  session.destroy
 end
